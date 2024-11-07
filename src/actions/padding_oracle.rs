da790f7c609a4c0005b75706e45ae0670aff6afd
Marco <s221887@student.dhbw-mannheim.de>
2024-11-07 03:06:18 +0100

    optimising performace: deleted stream.flush(), collected messages in a buffer before sending, allocating plaintext at the start

--- src/actions/padding_oracle.rs
+++ src/actions/padding_oracle.rs
@@ -4,7 +4,7 @@ use std::vec;
 
 pub fn execute(hostname: String, port: u32, mut iv: Vec<u8>, ciphertext: Vec<u8>) -> Result<Vec<u8>, io::Error> {
 
-    let mut plaintext = Vec::<u8>::new();
+    let mut plaintext = Vec::<u8>::with_capacity(ciphertext.len());
 
     for block in ciphertext.chunks(16).enumerate() {
 
@@ -15,13 +15,14 @@ pub fn execute(hostname: String, port: u32, mut iv: Vec<u8>, ciphertext: Vec<u8>
 
         // Input Ciphertext 
         stream.write(&block.1)?;
-        stream.flush()?;
 
         for (i, _byte) in block.1.iter().rev().enumerate() {
             intermediate_state[15-i] = correct_padding(&stream, &intermediate_state, 15 - i).unwrap();
         } 
+
+        let _ = stream.shutdown(std::net::Shutdown::Both);
         
-        for i in 0..intermediate_state.len() {
+        for i in 0..iv.len() {
             intermediate_state[i] = iv[i];
         }
 
@@ -43,20 +44,20 @@ fn correct_padding(mut stream: &TcpStream, intermediate_state: &Vec<u8>, byte_nu
     }
     
     // Create the 4-byte header (representing the number of 16-byte blocks sent to server)
-    const LENGHT: usize = 256;
-    let header = (LENGHT as u16).to_le_bytes();
+    const LENGTH: usize = 256;
+    let header = (LENGTH as u16).to_le_bytes();
 
     // Write Length to server
     stream.write(&header)?;
-    stream.flush()?;
 
-    for i in 0..LENGHT {
+    let mut message_buffer = Vec::with_capacity(LENGTH * 16);
+    for i in 0..LENGTH {
         q[byte_num] = i as u8;
-        stream.write_all(&q)?;
-        stream.flush()?;
+        message_buffer.extend_from_slice(&q);
     }
+    stream.write_all(&message_buffer)?;
 
-    let mut buffer = [0; LENGHT];
+    let mut buffer = [0; LENGTH];
     let _bytes_read = stream.read(&mut buffer)?;
 
     if byte_num != 15 {
@@ -76,16 +77,14 @@ fn correct_padding(mut stream: &TcpStream, intermediate_state: &Vec<u8>, byte_nu
 
                 // Write Length to server
                 stream.write(&header)?;
-                stream.flush()?;
 
                 // Prepare q
                 let mut q: Vec<u8> = vec![0; 16];
                 q[15] = i as u8;
-                q[14] = 1;
+                q[14] = 0xFF;
 
                 // Write TCP to server
                 stream.write_all(&q)?;
-                stream.flush()?;
                 
                 // Get resposne from server
                 let mut buffer = [0; 1];
